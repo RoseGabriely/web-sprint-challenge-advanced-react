dbc97f8a110fb4e0f1f05a2f37fe6898ed37963e
RoseGabriely <rosegabriely14@gmail.com>
2021-10-01 18:16:47 -0400

    added render without errors test

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,10 +1,13 @@
 import React from "react";
-import MutationObserver from 'mutationobserver-shim';
-import { render } from "@testing-library/react";
+import MutationObserver from "mutationobserver-shim";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import App from "../App";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("renders without errors", () => {});
+test("renders without errors", () => {
+  render(<App />);
+});
 
 test("shows success message on submit with form details", () => {});
