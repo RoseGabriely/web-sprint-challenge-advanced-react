1cb706e2fb02d2224b1e26ec933951cdaa5121f2
RoseGabriely <rosegabriely14@gmail.com>
2021-10-01 20:07:46 -0400

    checked that all inputs recieve valid data

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -14,6 +14,31 @@ test("renders without errors", () => {
 test("shows success message on submit with form details", () => {
   render(<CheckoutForm />);
 
+  const firstName = screen.getByLabelText("First Name:");
+  userEvent.type(firstName, "Rose");
+
+  expect(firstName).toHaveValue("Rose");
+
+  const lastName = screen.getByLabelText("Last Name:");
+  userEvent.type(lastName, "Gabriely");
+  expect(lastName).toHaveValue("Gabriely");
+
+  const address = screen.getByLabelText("Address:");
+  userEvent.type(address, "123 xy st.");
+  expect(address).toHaveValue("123 xy st.");
+
+  const city = screen.getByLabelText("City:");
+  userEvent.type(city, "NYC");
+  expect(city).toHaveValue("NYC");
+
+  const state = screen.getByLabelText("State:");
+  userEvent.type(state, "MA");
+  expect(state).toHaveValue("MA");
+
+  const zip = screen.getByLabelText("Zip:");
+  userEvent.type(zip, "00000");
+  expect(zip).toHaveValue("00000");
+
   const button = screen.getByRole("button");
   userEvent.click(button);
 
